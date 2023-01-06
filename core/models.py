e8d7cc22690e31231550f38ed45c8c738651d6be
ab7z <abhiksi613@gmail.com>
2023-01-06 09:50:32 +0530

    insert code is done, I didnt migrate the models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Database(models.Model):
+    id = models.UUIDFields(primary_key=true, default=uuid.uuid4)
+    input1 = models.CharField(max_length=100)
+    input2 = models.CharField(max_length=100)
+    input3 = models.CharField(max_length=100)
+    input4 = models.CharField(max_length=100)
+    input5 = models.CharField(max_length=100)
+    input6 = models.CharField(max_length=100)
+    input7 = models.CharField(max_length=100)
+    # input8 = models.CharField(max_length=100)
+    # input9 = models.CharField(max_length=100)
--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Database
 # Create your views here.
 
 
@@ -8,4 +9,29 @@ def index(request):
     return render(request, 'index.html')
 
 def insert(request):
-    return render(request, 'insert.html')
+    if request.method == "POST":
+        input1 = request.POST['input1']
+        input2 = request.POST['input2']
+        input3 = request.POST['input3']
+        input4 = request.POST['input4']
+        input5 = request.POST['input5']
+        input6 = request.POST['input6']
+        input7 = request.POST['input7']
+        # input8 = request.POST['input8']
+        # input9 = request.POST['input9']
+
+        db = Database.objects.create(
+            input1 = input1,
+            input2 = input2,
+            input3 = input3,
+            input4 = input4,
+            input5 = input5,
+            input6 = input6,
+            input7 = input7,
+            # input8 = input8,
+            # input9 = input9
+        )
+        db.save()
+
+        return redirect('/')
+    return HttpResponse('<h1>Some error occurred...</h1>')
